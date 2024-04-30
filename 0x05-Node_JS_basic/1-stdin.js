23ef8945c9c3cd18fb72210f75b27ff963a78911
root <RobinLeeHenry101@gmail.com>
2024-04-30 13:45:01 +0000

    fixed semi colon typo in task 1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-process.stdout.write("Welcome to Holberton School, what is your name?");
+process.stdout.write("Welcome to Holberton School, what is your name?\n");
 
 process.stdin.on('readable', () => {
-	const name = process.stdin.read():
+	const name = process.stdin.read();
 
 	if (name) {
 		process.stdout.write('Your name is: ${name}');
@@ -10,5 +10,5 @@ process.stdin.on('readable', () => {
 });
 
 process.stdin.on('end', () => {
-	process.stdout.write("This important software is now closing");
+	process.stdout.write("This important software is now closing\n");
 });
